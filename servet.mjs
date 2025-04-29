0fa8dc319178398935e91a560b3409bd8c0e08c3
sahanpramudith <pramuditsahan466@gmail.com>
2025-04-29 13:09:38 +0530

    feat: establish MongoDB connection before starting the server

--- servet.mjs
+++ servet.mjs
@@ -1,5 +1,6 @@
 import express,{ json } from "express"
 import "dotenv/config"
+import connectdb from "./src/model/user.mjs";
 
 console.log(process.env.MONGODB_URI);
 
@@ -7,6 +8,15 @@ const server=express()
 const PORT=process.env.PORT||4001
 server.use(json())
 
-server.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-})  
+
+
+connectdb.then(()=>{
+    console.log("Connected to MongoDB")
+    server.listen(PORT,()=>{
+        console.log(`Server is running on port ${PORT}`)
+    })  
+})
+.catch((err)=>{
+    console.log("Error connecting to MongoDB",err)
+}
+)
