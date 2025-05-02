78702d38951bc93020fbf58f9a3a311e7404208d
sahanpramudith <pramuditsahan466@gmail.com>
2025-05-02 09:16:53 +0530

    feat: implement user model with schema definition

---
+++ src/model/user.mjs
@@ -0,0 +1,12 @@
+
+import mongoose, { model } from "mongoose";
+
+const userSchema=new mongoose.Schema({
+    name:String,
+    username:String,
+    password:String,
+    email:String
+});
+
+const User=model(User,userSchema);
+export default User;
